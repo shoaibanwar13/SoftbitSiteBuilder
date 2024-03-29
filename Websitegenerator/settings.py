ba05062242db585f206636da90da91490a9f616b
Shoaib Anwar <124421743+shoaibanwar13@users.noreply.github.com>
2024-03-29 11:38:36 +0500

    Update settings.py

--- Websitegenerator/settings.py
+++ Websitegenerator/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4ko^6x&x(!-vzs^ehj$iduld85pucz@iycalk5k*ykqn^_$n%c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['softbit-website-builder.onrender.com']
 
 
 # Application definition
